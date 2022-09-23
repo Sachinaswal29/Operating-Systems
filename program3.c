ff7c4f0aaa41658ae554efc81e9d5ff01961070c
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2022-09-23 10:28:37 +0530

    Add files via upload

---
+++ program3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+    printf("Orphan Process:\n\n");
+    int x = fork();
+    if (x == 0)
+    {
+        printf("Child process id= %d\n", getpid());
+        printf("Parent process id= %d\n", getppid());
+    }
+    else
+    {
+        sleep(5);
+        printf("Process id of parent = %d\n", getpid());
+    }
+}
