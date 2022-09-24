cc77d19d0e5c2a91d1c1d5c7fbd489547bb19216
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2022-09-24 10:47:45 +0530

    Add files via upload

---
+++ program4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+    printf("Zombie Process:\n\n");
+    int x = fork();
+    if (x == 0)
+    {
+        printf("Process id of child: %d\n", getpid());
+        printf("Parent id of child: %d\n", getppid());
+    }
+    else
+    {
+        sleep(5);
+        printf("Process id of parent: %d\n", getpid());
+    }
+    return 0;
+}
